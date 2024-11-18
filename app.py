c24514d4992c1cca28520a0a2b7eb64b7c05cee1
tontontoto <yosssy0517@gmail.com>
2024-11-18 09:31:46 +0900

    ログイン機能1

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, Response
+from flask_sqlalchemy import SQLAlchemy
+from model_sample import db, User, Sale, Category, Bid, Like, Inquiry, WinningBid, Payment, PaymentWay, InquiryKind
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bcrypt import Bcrypt
+import os
+import hashlib
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sample.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.urandom(24)
+
+db.init_app(app)
+bcrypt = Bcrypt()
+#インスタンス化
+login_manager = LoginManager()
+#アプリをログイン機能を紐付ける
+login_manager.init_app(app)
+
+#現在のログインユーザーの情報を保持し、必要なときに参照できるようになる。
+@login_manager.user_loader
+def load_user(userId):
+    return User.query.get(userId)
+
+# def calculate_password_hash(password):
+#     text = password.encode('utf-8')
+#     result = hashlib.sha512(text).hexdigest()
+#     return result
+
+# ---- 新規登録ページ ----
+@app.route('/', methods=['GET', 'POST'])
+@login_required
+def index():
+    if request.method == 'GET':
+        user = User.query.all()
+        return render_template('index.html', user = user)
+
+
+# signup page ----
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        userName = request.form.get('userName')
+        displayName = request.form.get('displayName')
+        mailAddress = request.form.get('mailAddress')
+        password = request.form.get('password')
+
+        # passwordのハッシュ化
+        hashdPassword = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(userName=userName, displayName=displayName, mailAddress=mailAddress, password=hashdPassword) 
+
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+    else:
+        return render_template('signup.html')
+    
+# login-----
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        userName = request.form.get('userName')
+        password = request.form.get('password')
+        # Userテーブルからusernameに一致するユーザを取得
+        user = User.query.filter(User.userName==userName).first()
+        if user and bcrypt.check_password_hash(user.password, password):  # userがNoneでないかも確認
+            print("成功")
+            login_user(user)
+            return redirect('/')
+    else:
+        return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
+def add_data():
+	# model_sample.pyの仮データを挿入
+	with app.app_context():
+		db.create_all()
+		testUser = User(userName='testUser', displayName='testUser', mailAddress="test@gmail.com", password="abcde")
+		testUser2 = User(userName='testUser2', displayName='testUser2', mailAddress="test2@gmail.com", password="efghi")
+		db.session.add_all([testUser, testUser2])
+		db.session.commit()
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        add_data()
+    app.run(debug=True)
