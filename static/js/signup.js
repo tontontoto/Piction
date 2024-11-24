00483b0de8e3ccc92f029a42ad1076c6d5e7b7dc
tontontoto <yosssy0517@gmail.com>
2024-11-24 18:01:27 +0900

    ログイン機能_正規表現追加

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,22 +1,22 @@
 // 必要事項を入力していなかった時の処理。
 // ユーザーネーム、表示名、メールアドレス、パスワード、利用規約のチェックボックス
-
 function validateForm(event) {
     // エラーメッセージ要素を取得
     let errorMessages = document.getElementsByClassName('errorMassage');
 
     // 各フォームフィールドの値を取得
-    let userName = document.getElementById('userName').value;
-    let displayName = document.getElementById('displayName').value;
-    let mailAddress = document.getElementById('mailAddress').value;
-    let password = document.getElementById('password').value;
-    let checkBox = document.getElementById('privacyPolicy').checked; // チェックボックスの状態を取得
-
+    const userNameValue = document.getElementById('userName').value;
+    const displayNameValue = document.getElementById('displayName').value;
+    const mailAddressValue = document.getElementById('mailAddress').value;
+    const passwordValue = document.getElementById('password').value;
+    const checkBoxCheck = document.getElementById('privacyPolicy').checked; // チェックボックスの状態を取得
+    const validEmailAddresError = document.getElementById('validEmailAddresError');
     // 各フィールドが未入力ならエラーメッセージを表示
     let hasError = false;
 
     // ユーザーネームのチェック
-    if (userName === "") {
+    let userNameRegex = /^[a-zA-Z0-9_]{1,15}$/
+    if (!userNameRegex.test(userNameValue)) {
         errorMessages[0].style.display = "block";
         hasError = true;
     } else {
@@ -24,7 +24,7 @@ function validateForm(event) {
     }
 
     // 表示名のチェック
-    if (displayName === "") {
+    if (displayNameValue === "") {
         errorMessages[1].style.display = "block";
         hasError = true;
     } else {
@@ -32,15 +32,23 @@ function validateForm(event) {
     }
 
     // メールアドレスのチェック
-    if (mailAddress === "") {
+    if (mailAddressValue === "") {
         errorMessages[2].style.display = "block";
         hasError = true;
     } else {
         errorMessages[2].style.display = "none";
     }
+    // 正規表現
+    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(mailAddressValue)) {
+        // メールアドレスが無効な場合
+        validEmailAddresError.style.display = "block";
+        hasError = true;
+    }
 
     // パスワードのチェック
-    if (password === "") {
+    if (passwordValue === "") {
         errorMessages[3].style.display = "block";
         hasError = true;
     } else {
@@ -48,7 +56,7 @@ function validateForm(event) {
     }
 
     // 利用規約チェックボックスのチェック状態を確認
-    if (!checkBox) {
+    if (!checkBoxCheck) {
         errorMessages[4].style.display = "block";
         hasError = true;
     } else {
@@ -62,4 +70,33 @@ function validateForm(event) {
     }
 
     return true; // すべて正しい場合はフォームを送信
+}
+
+// 文字制限
+const userName = document.getElementById('userName');
+const inputCharacterError = document.getElementById('inputCharacterError');
+userName.addEventListener('input', function(){
+    const userNameInput = userName.value;
+    const validPattern = /^[a-zA-Z0-9_]*$/;
+    if (validPattern.test(userNameInput)) {
+        inputCharacterError.style.display = 'none';  // エラーメッセージを非表示
+    } else {
+        inputCharacterError.style.display = 'block';  // エラーメッセージを表示
+    }
+})
+// 有効メールアドレス制限
+
+
+
+// パスワード入力欄のパスワード表示非表示機能
+function pushHideButton() {
+    let password = document.getElementById("password");
+    let buttonEye = document.getElementById("buttonEye");
+    if(password.type === "text") {
+        password.type = "password";
+        buttonEye.className = "fa fa-eye-slash";
+    } else {
+        password.type = "text";
+        buttonEye.className = "fa fa-eye";
+    }
 }
