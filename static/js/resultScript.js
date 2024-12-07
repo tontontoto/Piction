9e80b7fe666378a19f0c62e886a76d8569fe25d8
yukio <yukio.y.2125@gmail.com>
2024-12-07 14:30:46 +0900

    モーダルウィンドウ＋経過時間表示

--- static/js/resultScript.js
+++ static/js/resultScript.js
@@ -1,85 +1,83 @@
-window.onload = function() {
-  const canvas = document.getElementById('myCanvas');
-  const context = canvas.getContext('2d');
-  const dataURL = localStorage.getItem('canvasImage');
+window.onload = function () {
+  const canvas = document.getElementById("myCanvas");
+  const context = canvas.getContext("2d");
+  const dataURL = localStorage.getItem("canvasImage");
   if (dataURL) {
-      const img = new Image();
-      img.onload = function() {
-          context.drawImage(img, 0, 0);
-      };
-      img.src = dataURL;
+    const img = new Image();
+    img.onload = function () {
+      context.drawImage(img, 0, 0);
+    };
+    img.src = dataURL;
   }
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-    const time = localStorage.getItem('elapsedTime');
-    const price = localStorage.getItem('moneyValue');
-    
-    if (time) {
-        document.querySelector('.timerResult p2').textContent = time;
-    }
-    
-    if (price) {
-        document.querySelector('.moneyResult p2').textContent = price;
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const time = localStorage.getItem("elapsedTime");
+  const price = localStorage.getItem("moneyValue");
+
+  if (time) {
+    document.querySelector(".timerResult p2").textContent = time;
+  }
+
+  if (price) {
+    document.querySelector(".moneyResult p2").textContent = price;
+  }
 });
 
-const open = document.getElementById('download');
-    open.addEventListener('click', () => {
-      container.addClass('active');
-      return false;
-    });
-
 //本保存
-const download = document.getElementById('download');
-download.addEventListener('click', () => {
+const download = document.getElementById("download");
+download.addEventListener("click", () => {
+  const time = localStorage.getItem("elapsedTime");
+  const price = localStorage.getItem("moneyValue");
+  const title = document.getElementById("title").value;
+  const dataURL = canvas.toDataURL("image/png"); // 画像をBase64に変換
 
-  const time = localStorage.getItem('elapsedTime');
-  const price = localStorage.getItem('moneyValue');
-  const title = document.getElementById('title').value;
-  const dataURL = canvas.toDataURL('image/png'); // 画像をBase64に変換
-  
-  localStorage.removeItem('canvasImage');
-  localStorage.removeItem('timerValue');
-  localStorage.removeItem('elapsedTime');
-  localStorage.removeItem('moneyValue');
-  
-  fetch('/add_sale', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({ title, image: dataURL, time: time, price: price}),
-    
-    }).then(response => {  // 画像保存の結果を表示
-      if (response.ok){
-        alert('正常に保存されました。');
+  localStorage.removeItem("canvasImage");
+  localStorage.removeItem("timerValue");
+  localStorage.removeItem("elapsedTime");
+  localStorage.removeItem("moneyValue");
+
+  fetch("/add_sale", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, image: dataURL, time: time, price: price }),
+  })
+    .then((response) => {
+      // 画像保存の結果を表示 デバック用
+      if (response.ok) {
+        alert("正常に保存されました。");
       } else {
-        alert('保存に失敗しました。');
+        alert("保存に失敗しました。");
       }
-    }).then(title => {
-      console.log(title);
     })
+    .then((title) => {
+      console.log(title);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  // 要素を取得
+  const close = document.querySelector(".modalClose");
+  const container = document.querySelector(".modalContainer");
+  const open = document.getElementById("openModal");
+
+  // openボタンをクリックしたらモーダルウィンドウを表示する
+  open.addEventListener("click", () => {
+    container.classList.add("active"); // クラスを追加
   });
 
-  $(function(){
-    // 変数に要素を入れる
-    var close = $('.modal-close'),
-    container = $('.modal-container');
-   
-    //読み込んで5秒後にモーダルウィンドウを表示
-    // setTimeout(() => {
-    //   container.addClass('active');
-    //   return false;
-    // },5000);
-    
-    //closeボタンをクリックしたらモーダルウィンドウを閉じる
-    close.on('click',function(){
-      container.removeClass('active');
-    });
-   
-    //モーダルウィンドウの外側をクリックしたらモーダルウィンドウを閉じる
-    $(document).on('click',function(e) {
-      if(!$(e.target).closest('.modal-body').length) {
-        container.removeClass('active');
-      }
-    });
+  // closeボタンをクリックしたらモーダルウィンドウを閉じる
+  close.addEventListener("click", () => {
+    container.classList.remove("active"); // クラスを削除
   });
+
+  // モーダルウィンドウの外側をクリックしたら閉じる
+  container.addEventListener("click", (e) => {
+    if (
+      !e.target.closest(".modalBody") &&
+      container.classList.contains("active")
+    ) {
+      container.classList.remove("active");
+    }
+  });
+});
