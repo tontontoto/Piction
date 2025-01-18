e30e29460738adbbe970a85cd0777c953251d738
yukio <yukio.y.2125@gmail.com>
2025-01-19 00:45:06 +0900

    gitignore修正

---
+++ model_sample.py
@@ -0,0 +1,163 @@
+from sqlalchemy import Column, ForeignKey, Table, String, Integer, Date, DATETIME, text, Boolean, ForeignKey, create_engine, func
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import date, datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+from os.path import join, dirname
+import pymysql
+import os
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DB_URL = os.environ.get("DB_URL")
+
+Base = declarative_base()
+engine = create_engine(DB_URL)
+db = SQLAlchemy()
+	
+saleCategoryAssociation = Table(
+    'saleCategoryAssociation', db.metadata,
+    Column('saleId', Integer, ForeignKey('sale.saleId'), primary_key=True),
+    Column('categoryId', Integer, ForeignKey('category.categoryId'), primary_key=True)
+)
+
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+    userId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userName = db.Column(db.String(15))
+    displayName = db.Column(db.String(10))
+    mailAddress = db.Column(db.String(254))
+    password = db.Column(db.String(254))
+    registrationDate = db.Column(db.Date, default=date.today())
+
+    sales= db.relationship("Sale", back_populates="user")
+    bids = db.relationship("Bid", back_populates="user") 
+    likes = db.relationship("Like", back_populates="user")
+    inquiries = db.relationship("Inquiry", back_populates="user")
+
+    winning_buyer = db.relationship("WinningBid", foreign_keys="[WinningBid.buyerId]", back_populates="buyer")
+    winning_seller = db.relationship("WinningBid", foreign_keys="[WinningBid.sellerId]", back_populates="seller")
+
+    def get_id(self):
+        return str(self.userId)
+
+
+class Category(db.Model):
+    __tablename__ = "category"
+    categoryId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    categoryName = db.Column(db.String(20))
+    sales = db.relationship("Sale", secondary=saleCategoryAssociation, back_populates="categories", lazy='dynamic')
+
+
+class Sale(db.Model):
+    __tablename__ = "sale"
+    saleId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userId = db.Column(db.Integer, ForeignKey('user.userId'))
+    displayName = db.Column(db.String(10), ForeignKey('user.displayName'))
+    title = db.Column(db.String(40), default="無題")
+    displayName = db.Column(db.String(10))
+    # categoryId = db.Column(db.Integer, ForeignKey('category.categoryId'))
+    filePath = db.Column(db.String(254))
+    startingPrice = db.Column(db.Integer)
+    currentPrice = db.Column(db.Integer)
+    creationTime = db.Column(db.String(5))
+    startingTime = db.Column(db.String(19))
+    finishTime = db.Column(db.String(19))
+    saleStatus = db.Column(db.Boolean, default=True)
+    listingTime = db.Column(db.String(19))
+
+    user = db.relationship("User", back_populates="sales")
+    categories = db.relationship("Category", secondary=saleCategoryAssociation, back_populates="sales", lazy='dynamic')
+    bids = db.relationship("Bid", back_populates="sale")
+    likes = db.relationship("Like", back_populates="sale")
+    payment = db.relationship("Payment", back_populates="sale", uselist=False)
+
+    @property
+    def like_count(self):
+        return Like.query.filter_by(saleId=self.saleId).count()
+
+# 入札テーブル
+class Bid(db.Model):    
+    __tablename__ = "bid"
+    bidId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    saleId = db.Column(db.Integer, ForeignKey('sale.saleId'))
+    userId = db.Column(db.Integer, ForeignKey('user.userId'))
+
+    bidPrice = db.Column(db.Integer)
+    bidTime = db.Column(db.DATETIME, default=datetime.now, nullable=False)
+
+    user = db.relationship("User", back_populates="bids")
+    sale = db.relationship("Sale", back_populates="bids")
+    winningBid = db.relationship("WinningBid", back_populates="bid", uselist=False)
+
+
+class WinningBid(db.Model):
+    __tablename__ = "winningBid"
+    winningBidId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    buyerId = db.Column(db.Integer, ForeignKey('user.userId'))
+    sellerId = db.Column(db.Integer, ForeignKey('user.userId'))
+    bidId = db.Column(db.Integer, ForeignKey('bid.bidId'))
+
+    buyer = db.relationship("User", foreign_keys=[buyerId], back_populates="winning_buyer") 
+    seller = db.relationship("User", foreign_keys=[sellerId], back_populates="winning_seller") 
+    bid = db.relationship("Bid", back_populates="winningBid")
+    payment = db.relationship("Payment", backref="winningBid", uselist=False)
+    
+
+class PaymentWay(db.Model):
+    __tablename__ = "paymentWay"
+    paymentWayId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    paymentWayName = db.Column(db.String(20))
+    payments = db.relationship("Payment", back_populates="paymentWay")
+
+
+class Payment(db.Model):
+    __tablename__ = "payment"
+    paymentId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    saleId = db.Column(db.Integer, ForeignKey('sale.saleId'))
+    winningBidId = db.Column(db.Integer, ForeignKey('winningBid.winningBidId'))
+    paymentWayId = db.Column(db.Integer, ForeignKey('paymentWay.paymentWayId'))
+    paymentDate = db.Column(db.DATETIME, default=datetime.now(), nullable=False)
+
+    sale = db.relationship("Sale", back_populates="payment")
+    paymentWay = db.relationship("PaymentWay", back_populates="payments")
+    amount = db.Column(db.Integer)
+
+
+class Like(db.Model):
+    __tablename__ = "like"
+    likeId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userId = db.Column(db.Integer, ForeignKey('user.userId'))
+    saleId = db.Column(db.Integer, ForeignKey('sale.saleId'))
+
+    user = db.relationship("User", back_populates="likes")
+    sale = db.relationship("Sale", back_populates="likes")
+
+class InquiryKind(db.Model):
+    __tablename__ = "inquiryKind"
+    inquiryKindId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    inquiryKindName = db.Column(db.String(10))
+
+    inquiries = db.relationship("Inquiry", back_populates="inquiryKind")
+
+class Inquiry(db.Model):
+    __tablename__ = "inquiry"
+    inquiryId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userId = db.Column(db.Integer, ForeignKey('user.userId'))
+    displayName = db.Column(db.String(10))
+    mailAddress = db.Column(db.String(254))
+    inquiryKindId = db.Column(db.Integer, ForeignKey('inquiryKind.inquiryKindId'))
+    appendFile = db.Column(db.String(30))
+    inquiryContent = db.Column(db.String(500))
+    inquiryAnswer = db.Column(db.String(500))
+
+    user = db.relationship("User", back_populates="inquiries")
+    inquiryKind = db.relationship("InquiryKind", foreign_keys=[inquiryKindId], back_populates="inquiries")
+
---
+++ static/js/script.js
@@ -0,0 +1,190 @@
+const canvas = document.getElementById("myCanvas");
+const stage = new createjs.Stage(canvas);
+
+const size = Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8);
+canvas.style.width = `${size}px`; // 見た目のサイズ
+canvas.style.height = `${size}px`; // 見た目のサイズ
+canvas.width = size; // 描画解像度（内部ピクセル数）
+canvas.height = size;
+
+window.addEventListener("resize", () => {
+  const size = Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8);
+  canvas.style.width = `${size}px`;
+  canvas.style.height = `${size}px`;
+  canvas.width = size;
+  canvas.height = size;
+});
+
+function drawBackground() {
+  const background = new createjs.Shape();
+  background.graphics.beginFill("white").drawRect(0, 0, canvas.width, canvas.height);
+  stage.addChild(background);
+  stage.update();
+}
+
+drawBackground();
+
+const lineWidthInput = document.getElementById("lineWidth");
+const lineOpacityInput = document.getElementById("lineOpacity");
+
+const line = new createjs.Shape();
+stage.addChild(line);
+stage.update();
+
+// 初期の太さ
+let lineWidth = parseInt(lineWidthInput.value);
+let lineOpacity = parseFloat(lineOpacityInput.value);
+// 線の太さ更新
+lineWidthInput.addEventListener("input", () => {
+  lineWidth = parseInt(lineWidthInput.value);
+});
+lineOpacityInput.addEventListener("input", () => {
+  lineOpacity = parseFloat(lineOpacityInput.value);
+});
+
+let isDrawing = false;
+let isEraserActive = false;
+
+const penButton = document.getElementById("pen");
+penButton.addEventListener("click", () => {
+  isEraserActive = false; // ペンに切り替え
+  if (!isEraserActive) {
+    penButton.style.backgroundColor = "#ededed";
+    penButton.style.transform = "translateX(5px)";
+    penButton.style.zIndex = "0";
+
+    eraserButton.style.backgroundColor = "";
+    eraserButton.style.transform = "";
+  }
+});
+
+// 消しゴムボタンのクリックイベント
+const eraserButton = document.getElementById("eraser");
+eraserButton.addEventListener("click", () => {
+  isEraserActive = true; // 消しゴム切り替え
+  if (isEraserActive) {
+    eraserButton.style.backgroundColor = "#ededed";
+    eraserButton.style.transform = "translateX(5px)";
+    eraserButton.style.zIndex = "0";
+
+    penButton.style.backgroundColor = "";
+    penButton.style.transform = "";
+  }
+});
+
+// クリアボタンのクリックイベント
+const clearButton = document.getElementById("reset");
+clearButton.addEventListener("click", () => {
+  var result = confirm("リセットしてもよろしいですか？"); // 確認ダイアログ
+  if(result == true){
+    line.graphics.clear();
+    stage.update();
+  }
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  const rect = canvas.getBoundingClientRect();
+  const mouseX = e.clientX - rect.left; 
+  const mouseY = e.clientY - rect.top;
+  
+
+  if (isEraserActive) {
+    // 消しゴムの設定
+    line.graphics.setStrokeStyle(lineWidth, 1, "round").beginStroke("white").moveTo(mouseX, mouseY);
+  } else {
+    // 通常のペン設定
+    const paintColorHex = document.querySelector("#inputColor").value;
+
+    // 色を16進数から10進数のRGBに変換
+    const paintColorRGB = parseInt(paintColorHex.slice(1), 16); // #を除去して数値に変換
+    line.graphics
+    .setStrokeStyle(lineWidth, 1, "round")
+    .beginStroke(createjs.Graphics.getRGB(paintColorRGB, lineOpacity))
+    .moveTo(mouseX, mouseY)
+    .lineTo(mouseX, mouseY);
+  }
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (!isDrawing) return;
+  const rect = canvas.getBoundingClientRect();
+  const mouseX = e.clientX - rect.left;
+  const mouseY = e.clientY - rect.top;
+  line.graphics.lineTo(mouseX, mouseY);
+  stage.update();
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+  line.graphics.endStroke();
+  stage.update();
+});
+
+
+
+// タイマー
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+
+      // 経過時間の計算
+      var elapsed = duration - timer;
+
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      // 経過時間をローカルストレージに保存
+      localStorage.setItem('elapsedTime', elapsed);
+
+      if (--timer < 0) { // タイマーが0になったら内容の保存＆リダイレクト
+        saveCanvas();
+        window.location.href = '/result';
+      }
+  }, 1000);
+    
+}
+
+// 値段の変化
+let moneyElement = document.querySelector('.money p');
+let money = 502;
+
+function updateMoney() {
+    if (moneyElement && money > 0) {
+        money -= 2;
+        moneyElement.textContent = money + '円';
+    }
+}
+
+setInterval(updateMoney, 1000);
+
+window.onload = function () {
+  var threeMinutes = 60 * 3,
+      display = document.querySelector('.timer p');
+  startTimer(threeMinutes, display);
+};
+
+// 描画内容の仮保存
+function saveCanvas() {
+  var canvas = document.getElementById('myCanvas');
+  var dataURL = canvas.toDataURL();
+  localStorage.setItem('canvasImage', dataURL);
+
+  // タイマーと値段を保存
+  var timerValue = document.querySelector('.timer p').textContent;
+  localStorage.setItem('timerValue', timerValue);
+  localStorage.setItem('moneyValue', money);
+}
+
+//ボタンをクリックしたときに保存する
+window.addEventListener('DOMContentLoaded', (event) => {
+  const saveButton = document.getElementById('saveButton');
+  if (saveButton) {
+    saveButton.addEventListener('click', saveCanvas);
+  }
+});
